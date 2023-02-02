aca207672552419c028a92703c0a21dd63640f64
Povilas <zdanis.povilas@gmail.com>
2023-02-02 12:15:54 +0200

    Initial Commit

---
+++ src/test/java/io/inventi/accountmanagement/AccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package io.inventi.accountmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
